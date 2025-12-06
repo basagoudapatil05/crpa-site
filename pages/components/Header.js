2cc1a8b27e26dcf795fb456f03c75e3b0067122e
basagoudapatil05 <basagoudapatil05@gmail.com>
2025-12-06 10:31:16 +0530

    Create Header.js

---
+++ pages/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header style={{ padding: 20, background: "#111", color: "#ccc" }}>
+      <h2>C. R. PATIL & ASSOCIATES</h2>
+      <p>Structure. Craft. Assurance.</p>
+    </header>
+  );
+}
