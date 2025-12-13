88eecf2125eeae54ada790318a319c7f6856c0ea
basagoudapatil05 <basagoudapatil05@gmail.com>
2025-12-13 12:55:21 +0530

    Create school.js

---
+++ pages/school.js
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+import ProjectCard from "../components/ProjectCard";
+
+export default function SchoolProjects() {
+  const [projects, setProjects] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/projects/list")
+      .then((r) => r.json())
+      .then((data) => {
+        setProjects(data.filter((p) => p.category === "school"));
+      });
+  }, []);
+
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>School Projects</h1>
+
+      {projects.length === 0 && <p>No school projects found.</p>}
+
+      {projects.map((p) => (
+        <ProjectCard key={p.id} project={p} />
+      ))}
+    </div>
+  );
+}
