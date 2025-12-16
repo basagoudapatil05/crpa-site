475379051017909595feb6bfc4a49ed0d80c4b80
basagoudapatil05 <basagoudapatil05@gmail.com>
2025-12-16 13:19:07 +0530

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { createClient } from "lib/supabase.js";
-
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
